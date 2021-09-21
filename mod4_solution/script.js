6c0e137f6c625d2450c3d89a4e619453356943f6
System Administrator <root@Alis-MacBook-Air.local>
2021-09-21 18:44:01 +0430

    assignment4 done

---
+++ mod4_solution/script.js
@@ -0,0 +1,14 @@
+(function(window) {
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+    for (index in names) {
+        var firstLetter = names[index].charAt(0).toLowerCase();
+        
+        if (firstLetter == 'j') {
+            byeSpeaker.speak(names[index]);
+        } else {
+            helloSpeaker.speak(names[index]);
+        }
+    }
+
+})(window);
